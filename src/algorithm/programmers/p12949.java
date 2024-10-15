3c846fe81c26f932ddb8af0ab734f46b161eba22
baekchan1024 <baekchan1024@gmail.com>
2024-10-15 19:25:08 +0900

    프로그레머스 문제

---
+++ src/algorithm/programmers/p12949.java
@@ -0,0 +1,34 @@
+package algorithm.programmers;
+
+import java.util.Arrays;
+
+public class p12949 {
+
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int r1 = arr1.length;
+        int c1 = arr1[0].length;
+        int r2 = arr2.length;
+        int c2 = arr2[0].length;
+        int[][] answer = new int[r1][c2];
+
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                for (int k = 0; k < c1; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] input1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] input2 = {{3, 3}, {3, 3}};
+        int[][] solution = solution(input1, input2);
+        for (int i = 0; i < solution.length; i++) {
+            for (int j = 0; j < solution[i].length; j++) {
+                System.out.print(solution[i][j] + " ");
+            }
+        }
+    }
+}
