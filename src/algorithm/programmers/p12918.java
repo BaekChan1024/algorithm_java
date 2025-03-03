5ca9a0425bf2df4792918bf1ea45033035f09b37
baekchan1024 <baekchan1024@gmail.com>
2025-03-03 13:15:46 +0900

    문자열 다루기 기본

---
+++ src/algorithm/programmers/p12918.java
@@ -0,0 +1,16 @@
+package algorithm.programmers;
+
+public class p12918 {
+
+    public boolean solution(String s) {
+        if(s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
