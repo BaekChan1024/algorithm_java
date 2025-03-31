60f743756d6a24a8f9c19d7a673476f932da6d78
baekchan1024 <baekchan1024@gmail.com>
2025-03-31 16:14:23 +0900

    [프로그래머스] 행렬의 곱셈

---
+++ src/algorithm/array/programmers/행렬의_곱셈/p12949.java
@@ -0,0 +1,33 @@
+package algorithm.array.programmers.행렬의_곱셈;
+
+public class p12949 {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
+        int[][] result = s.solution(arr1, arr2);
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[i].length; j++) {
+                for (int k = 0; k < arr2.length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+
+            }
+        }
+        return answer;
+    }
+}
